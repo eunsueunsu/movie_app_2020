6118194af9e1f7390d15cd977ac5444a7bd2b3a6
eunsu lee <54570195+eunsueunsu@users.noreply.github.com>
2020-11-01 13:40:18 +0900

    #2.3

--- src/App.js
+++ src/App.js
@@ -3,27 +3,38 @@ import React from "react";
 function Food({ name, picture }) {
   return( 
   <div>
-   <h1>I love {name}</h1>
-   <img src= { picture }  />
+   <h1>I like {name}</h1>
+   <img src= { picture } />
    </div> )
 }
 // 동적 데이터가 있는 컴포넌트
 
 const foodILike =[
   {
+    id: "1",
   name: "kimchi",
-  image: "http://www.11st.co.kr/connect/Gateway.tmall?method=Xsite&prdNo=2003400266&tid=1001366138"
-},
+image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Carabao_mangoes_%28Philippines%29.jpg/220px-Carabao_mangoes_%28Philippines%29.jpg"},
 {
+  id : "2",
   name: "mango",
-  image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Ff%2Ffb%2FCarabao_mangoes_%2528Philippines%2529.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCarabao_(mango)&tbnid=AGpPMB8I0xGCQM&vet=12ahUKEwi5zJ_3tcjsAhUQCpQKHStjA_sQMygDegUIARDqAQ..i&docid=nChwq7hvtTHg3M&w=2172&h=1988&q=mango&ved=2ahUKEwi5zJ_3tcjsAhUQCpQKHStjA_sQMygDegUIARDqAQ"
-}
+  image: "https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta-650x650.jpg"
+},
+// {
+//   eunsu : "eunsu"
+// }
 ]
+
+function renderFood(dish){
+  console.log(dish);
+  return <Food key={dish.id} name={dish.name} picture={dish.image}/>
+}
 function App() {
   return (
     <div>
-      <h1>hello</h1>
-      {foodILike.map(dish => (<Food name={dish.name} picture={dish.image}/>))}
+      {/* <h1>hello</h1> */}
+       {/* dish : just item , normal object of map */}
+      {/* {foodILike.map(dish => (<Food name={dish.name} picture={dish.image}/>))} */}
+      {foodILike.map(renderFood)}
     </div>
   )
 
